95503d27f6817ed04c42e85cbeb002a114fe0ce3
smallgreycreatures <smallgreycreatures@gmail.com>
2014-02-03 17:43:41 +0100

    Create TestProgram.java

---
+++ TestProgram.java
@@ -0,0 +1,12 @@
+public class TestProgram{
+
+  public TestProgram(){
+    System.out.println("Hello World!");
+  }
+  
+  public static void main(String[]args){
+  
+    new TestProgram();
+    
+    }
+  }
