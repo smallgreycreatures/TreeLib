f8382c60d52a4e07986161ee2fd13e31722ddf45
roek5803 <robin.eklund23@gmail.com>
2014-02-04 18:20:59 +0100

    Update TestProgram.java

--- TestProgram.java
+++ TestProgram.java
@@ -1,12 +1,29 @@
-public class TestProgram{
 
-  public TestProgram(){
-    System.out.println("Hello World!");
-  }
-  
-  public static void main(String[]args){
-  
-    new TestProgram();
-    
-    }
-  }
+public class PresetFinder{
+	
+	public static void main(String[] args){
+		
+		Tree<String> tree = new Tree<String>("Factory"); 
+	
+		tree.addType("Bass"); 
+		tree.addType("Synth Pad"); 
+		tree.addType("Synth Lead"); 
+		tree.addType("Synth Basic"); 
+		tree.addType("Synth Bit"); 
+		tree.addType("Plucks"); 
+		tree.addType("Acustic"); 
+		
+		System.out.println(tree.size());
+		
+		tree.getTypes();
+		
+		System.out.println(tree.contains("Synth Lead"));
+		
+		tree.addSubType("Sub Bass", tree.getType("Bass"));
+		tree.addSubType("Fingered Bass", tree.getType("Bass"));
+		
+		System.out.println(tree.getType("Bass").firstChild.nextSibling.data);
+		
+	}
+	
+}
